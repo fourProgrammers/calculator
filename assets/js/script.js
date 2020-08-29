f36ae6402f1974d9a519075dbf458fc98f646bfa
samieel <sotton.maia@gmail.com>
2020-08-29 17:07:56 -0300

    add operation +

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+window.onload = () => {
+    const sumButton = document.getElementById("sumButton");
+    const subButton = document.getElementById("subButton");
+    const multButton = document.getElementById("multButton");
+    const divButton = document.getElementById("divButton");
+    const clearButton = document.getElementById("clearButton");
+    const deleteButton = document.getElementById("deleteButton");
+    const plusMinusButton = document.getElementById("plusMinusButton");
+    const percentageButton = document.getElementById("percentageButton");
+
+
+    // clear all inputs
+    clearButton.onclick = () => {
+        document.getElementById("term").value = "0";
+        document.getElementById("label").innerText = " ";
+        document.getElementById("result").innerText = "0";
+    }
+    
+    deleteButton.onclick = () => {
+        valor = document.getElementById("term").value;
+        if (valor.length == 1){
+            document.getElementById("term").value = "0";
+        }else{
+            document.getElementById("term").value = valor.slice(0, -1);
+        }
+    }
+    
+    plusMinusButton.onclick = () =>{
+        valor = document.getElementById("term").value;
+        document.getElementById("term").value = parseInt(valor) * (-1);
+    }
+    
+    percentageButton.onclick = () =>{
+        valor = document.getElementById("term").value;
+        document.getElementById("term").value = parseFloat(valor) / 100;
+    }
+}
+
+addNumber = (element) => {
+
+    const term = document.getElementById("term");
+
+    if (term.value === '0') {
+        if(element.getAttribute('data-numero') == '.'){
+            term.value += element.getAttribute('data-numero');
+        } else {
+            term.value = element.getAttribute('data-numero');
+        }
+
+    } else {
+        //limite de 8 caracteres para o texto ficar arrumado
+        if(term.value.length < 8) {
+            if(term.value.includes('.') != true && element.getAttribute('data-numero') == '.') {
+                term.value += element.getAttribute('data-numero');
+            } else if(element.getAttribute('data-numero') != '.') {
+                term.value += element.getAttribute('data-numero');
+            }
+        }
+    }
+}
+
+addOperation = (element) => {
+
+    const operation = element.getAttribute('data-operation');
+
+    const term = document.getElementById("term");
+    const label = document.getElementById("label");
+    const result = document.getElementById("result");
+
+    label.innerText += (term.value + element.getAttribute('data-operation'));
+
+    if(operation == "+") {
+        const aux = parseInt(result.innerText) + parseInt(term.value);
+        result.innerText = aux;
+    }
+
+    term.value = '0';
+}
